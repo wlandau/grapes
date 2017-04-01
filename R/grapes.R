643b388afbaa550b97b550ea58d6601b1c1e7c04
Will Landau <will.landau@gmail.com>
2017-03-31 21:14:18 -0400

    Add full functionality and appveyor

---
+++ R/grapes.R
@@ -0,0 +1,52 @@
+#' @title Function \code{grapes}
+#' @description Turn a collection of functions into binary operators. For
+#' example, create the binary operator \code{\%cbind\%} from the function
+#' \code{\link{cbind}()} by calling \code{grapes(cbind)}. Then,
+#' you can call \code{sleep \%cbind\% sleep \%cbind\% sleep} to add
+#' to the columns of a data frame while avoiding cumbersome parentheses.
+#' @export
+#' @param ... characters or symbols, names of functions to make into
+#' binary operators.
+#' @param list character vector of names of functions to make into
+#' binary operators
+#' @param from either a character scalar or an environment. If an environment,
+#' this is where \code{grapes()} will look for the the functions named in 
+#' \code{...} and \code{list}. If a character string, \code{from} names the 
+#' package where \code{grapes()} will look for functions.
+#' @param to environment to store the new binary operators. 
+#' @examples
+#' grapes(rbind, from = "base") # 'from' defaults to your current workspace
+#' nrow(sleep) # 20
+#' longer = sleep %rbind% sleep %rbind% sleep # No clumsy parentheses!
+#' nrow(longer) # 60. We'd all like to sleep longer.
+grapes = function(..., list = character(0), from = to, to = parent.frame()){
+  dots = match.call(expand.dots = FALSE)$...
+  names = vapply(dots, as.character, "") %>% c(list)
+  force(from)
+  force(to)
+  from = get_from(from)
+  lapply(names, grape, from = from, to = to)
+  invisible()
+}
+
+grape = function(name, from, to){
+  value = from[[name]]
+  if(is.function(value))
+    assign(x = grape_name(name), value = value, envir = to)
+}
+
+grape_name = function(name){
+  paste0("%", name, "%")
+}
+
+get_from = function(from){
+  if(is.character(from)){
+    require(from, character.only = TRUE)
+    from = paste0("package:", from) %>%
+      as.environment
+  }
+  if(!is.environment(from))
+    stop("argument 'from' must be a package name or an environment.")
+  from
+}
+
